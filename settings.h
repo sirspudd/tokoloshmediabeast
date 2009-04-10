6d3a778213607d2772fde904c07c64f54eb1537b
Anders Bakken <anders.bakken@nokia.com>
2009-04-10 14:05:47 -0700

    add settings thing that will do clever templates

---
+++ settings.h
@@ -0,0 +1,24 @@
+#ifndef SETTINGS_H
+#define SETTINGS_H
+
+#include <QString>
+class Settings
+{
+public:
+    static template <class T>  T value(const QString &key)
+    {
+        if (
+
+
+    }
+
+    static template <class T> void setValue(const QString &key, const T &t)
+    {
+
+    }
+private:
+    Settings() {}
+};
+
+
+#endif
