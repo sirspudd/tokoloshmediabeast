e73d2262271e6815fc4bb338aed8b36bcee16ff8
Donald Carr <sirspudd@gmail.com>
2009-04-24 08:54:45 +0000

    - Add in translation stuff since it is free, just need to handle   install/uninstalling them gracefully again which is a task for another   day :)
    Signed-off-by: Donald Carr <sirspudd@gmail.com>

---
+++ tokoloshhead/i18n/tokoloshhead.de.ts
@@ -0,0 +1,128 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>Player</name>
+    <message>
+        <location filename="../player.cpp" line="137"/>
+        <source>Previous</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../player.cpp" line="139"/>
+        <source>Play</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../player.cpp" line="141"/>
+        <source>Pause</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../player.cpp" line="143"/>
+        <source>Stop</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../player.cpp" line="145"/>
+        <source>Next</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../player.cpp" line="148"/>
+        <source>Open</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../player.cpp" line="150"/>
+        <source>OpenSkin</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../player.cpp" line="153"/>
+        <source>Shuffle</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../player.cpp" line="155"/>
+        <source>Repeat</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../player.cpp" line="158"/>
+        <source>Equalizer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../player.cpp" line="160"/>
+        <source>Playlist</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../player.cpp" line="215"/>
+        <source>Quit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../player.cpp" line="216"/>
+        <source>Edit shortcuts</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../player.cpp" line="218"/>
+        <source>Toggle debug geometry</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../player.cpp" line="221"/>
+        <source>Toggle overlay</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../player.cpp" line="449"/>
+        <source>Open files</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../player.cpp" line="451"/>
+        <source>Music files(*.mp3 *.ogg *.flac)</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ShortcutDialog</name>
+    <message>
+        <location filename="../shortcutdialog.cpp" line="14"/>
+        <source>Shortcut Dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../shortcutdialog.cpp" line="18"/>
+        <source>Action</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../shortcutdialog.cpp" line="83"/>
+        <source>Add shortcut</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../shortcutdialog.cpp" line="84"/>
+        <source>Revert to default</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../shortcutdialog.cpp" line="86"/>
+        <source>Remove shortcut</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SkinSelectionDialog</name>
+    <message>
+        <location filename="skinSelectionDialog.cpp" line="31"/>
+        <source>Select skin</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
