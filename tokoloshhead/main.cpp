ee8784b01f68229c68f98760bd86c104595114ad
Anders Bakken <anders.bakken@nokia.com>
2009-07-06 08:06:35 -0700

    Remove some debugs

--- tokoloshhead/main.cpp
+++ tokoloshhead/main.cpp
@@ -91,20 +91,16 @@ int main(int argc, char *argv[])
                 QList<Function> functions;
                 functions.append(readDBusMessage<Function>(interface->call("findFunction", arg)));
                 QString error;
-                qDebug() << functions.value(0).name;
                 foreach(const Function &f, functions) {
-                    qDebug() << f.name;
                     if (argc - i - 1 < f.args.size()) {
                         error = QString("Not enough arguments specified for %1 needed %2, got %3").
                                 arg(f.name).arg(f.args.size()).arg(argc - i - 1);
-                        qDebug() << error;
+                        qDebug("%s", qPrintable(error));
                         continue;
                     }
-                    qDebug() << f.args.size();
 
                     QList<QVariant> arguments;
                     int ii = i;
-                    qDebug() << f.args;
                     for (int j=0; j<f.args.size(); ++j) {
                         QVariant variant = args.at(++ii);
                         if (!variant.convert(static_cast<QVariant::Type>(f.args.at(j)))) {
@@ -116,7 +112,6 @@ int main(int argc, char *argv[])
                     if (!error.isEmpty())
                         continue;
                     i = ii;
-                    qDebug() << arguments;
                     const QDBusMessage ret = interface->callWithArgumentList(QDBus::Block, f.name, arguments);
                     // ### what if it can't call the function?
                     if (!ret.arguments().isEmpty())
